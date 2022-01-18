f87f6c120788f88522b82b31d5b4691d91b1c2ac
Gartner, David <david.gartner@tsi.com>
2022-01-17 23:02:56 -0600

    Working with a couple quirks

--- crawler.js
+++ crawler.js
@@ -4,8 +4,7 @@ export class Crawler {
 		this.ns = ns;
 		this.recurseDepth = 0;
 		this.hostname = this.ns.getHostname();
-		this.serverNames = [this.hostname];
-		this.servers = [ns.getServer(this.hostname)];
+		this.servers = [this.hostname];
 	}
 
 	async run() {
@@ -16,21 +15,20 @@ export class Crawler {
 	async recursiveScan(server)
 	{
 		this.recurseDepth++;
-		var subTargets = this.ns.scan(server.hostname);
+		var subTargets = this.ns.scan(server);
 		//ns.tprint("Scanning from: " + target);
 		
 		for(let i = 0; i < subTargets.length; i++)
 		{
 			var sTarget = subTargets[i];
 			var server = this.ns.getServer(sTarget);
-			if(this.serverNames.includes(sTarget))
+			if(this.servers.includes(sTarget))
 			{
 				continue;
 			}
 			else{
-				this.servers.push(server);
-				this.serverNames.push(sTarget);
-				await this.recursiveScan(server);
+				this.servers.push(sTarget);
+				await this.recursiveScan(sTarget);
 			}
 		}
 		this.recurseDepth--;
@@ -44,28 +42,28 @@ export class Crawler {
 	nukeServer(server) {
 		var openedPorts = 0;
 		if (this.ns.fileExists("BruteSSH.exe", "home")) {
-			this.ns.brutessh(server.hostname);
+			this.ns.brutessh(server);
 			openedPorts++;
 		}
 		if (this.ns.fileExists("FTPCrack.exe", "home")) {
-			this.ns.ftpcrack(server.hostname);
+			this.ns.ftpcrack(server);
 			openedPorts++;
 		}
 		if (this.ns.fileExists("relaySMTP.exe", "home")) {
-			this.ns.relaysmtp(server.hostname);
+			this.ns.relaysmtp(server);
 			openedPorts++;
 		}
 		if (this.ns.fileExists("HTTPWorm.exe", "home")) {
-			this.ns.httpworm(server.hostname);
+			this.ns.httpworm(server);
 			openedPorts++;
 		}
 		if (this.ns.fileExists("SQLInject.exe", "home")) {
-			this.ns.sqlinject(server.hostname);
+			this.ns.sqlinject(server);
 			openedPorts++;
 		}
-		if(this.ns.getServerNumPortsRequired(server.hostname) <= openedPorts){
-			var result = this.ns.nuke(server.hostname) ? "SUCCESS" : "FAILURE";
-			this.ns.tprint("Nuking " + server.hostname + ": " + result);
+		if(this.ns.getServerNumPortsRequired(server) <= openedPorts){
+			var result = this.ns.nuke(server) ? "SUCCESS" : "FAILURE";
+			this.ns.tprint("Nuking " + server + ": " + result);
 			return result == "SUCCESS";
 		}
 		return false;
@@ -81,17 +79,47 @@ export class Crawler {
 		let tempservers = [];
 		for(let i = 0; i < this.servers.length; i++)
 		{
-			let server = this.servers[i];
+			let server = this.ns.getServer(this.servers[i]);
 			if(server.moneyMax > 0)
 			{
-				tempservers.push(server);
+				if(server.requiredHackingSkill < (this.ns.getPlayer().hacking * 0.25))
+				{
+					tempservers.push(server.hostname);
+				}
 			}
 		}
 		return tempservers;
+	}
+
+	async infestAll()
+	{
+		for(let i = 0; i < this.servers.length; i++)
+		{
+			await this.infest(this.servers[i]);
+		}
+	}
+
+	async infest(server)
+	{
+		let serv = this.ns.getServer(server);
+		if(serv.hasAdminRights)
+		{
+			let threads = serv.maxRam / this.ns.getScriptRam("basic-hack.js");
+			if(threads > 0)
+			{		
+				await this.ns.scp("basic-hack.js", server);
+				this.ns.tprint("Basic Hacking " + server);
+				this.ns.exec("basic-hack.js", server, threads, server);
+			}
+		}
 	}
 }
 
 /** @param {NS} ns **/
 export async function main(ns) {
 	var crawler = new Crawler(ns);
+	await crawler.run();
+	crawler.nukeAll();
+	await crawler.run();
+	await crawler.infestAll();
 }
--- godhack.js
+++ godhack.js
@@ -16,12 +16,19 @@ export async function main(ns) {
     {
         for(let i = 0; i < servers.length; i++)
         {
+            //ns.tprint("Working on " + servers[i]);
             let server = servers[i];
+            if(analyzer.isSaturated(server))
+            {
+                ns.tprint(server + " is saturated");
+                continue;
+            }
             analyzer.weaken(server);
             analyzer.grow(server);
             analyzer.hack(server);
+            break;
         }
-        await ns.sleep(1);
+        await ns.sleep(5);
     }
 }
 
@@ -31,115 +38,144 @@ class Analyzer
     {
         this.ns = ns;
         this.tm = tm;
-        this.homeServer = ns.getServer(ns.getHostname());
-        this.cores = this.homeServer.cpuCores;
+        this.homeServer = ns.getHostname();
+        this.cores = this.ns.getServer(this.homeServer).cpuCores;
+        this.pid = 0;
+    }
+
+    isSaturated(server)
+    {
+        let wt = this.getWeakenThreads(server);
+        let gt = this.getGrowThreads(server);
+        let ht = this.getHackThreads(server);
+        let isSaturated = false;
+        if(wt == 0 && gt == 0 && ht == 0)
+        {
+            isSaturated = true;
+        }
+        return isSaturated;
     }
 
     weaken(server)
     {
+        //this.ns.tprint("Trying weaken on " + server);
         let scriptName = "weaken.js";
-        let endTime = this.ns.getWeakenTime(server.hostname);
+        let endTime = this.ns.getWeakenTime(server);
         let threads = this.getWeakenThreads(server);
+        //this.ns.tprintf("  Ideal threads = %d", threads);
         threads = Math.min(this.getMaxThreads(scriptName), threads);
         let secEffect = 0 - this.ns.weakenAnalyze(threads, this.cores);
         let moneyMultiplier = 1;
         if(threads > 0)
         {
-            this.ns.exec(scriptName, this.ns.getHostname(), threads);
-            this.tm.push(new Task(server.hostname, scriptName, secEffect, moneyMultiplier, endTime, threads))
+            this.ns.exec(scriptName, this.homeServer, threads, server, this.pid++);
+            this.tm.push(new Task(server, scriptName, secEffect, moneyMultiplier, endTime, threads));
+            this.ns.print(this.ns.sprintf("Spawning %d %s threads on %s", threads, scriptName, server));
         }
     }
 
     grow(server)
     {
         let scriptName = "grow.js";
-        let endTime = this.ns.getGrowTime(server.hostname);
+        let endTime = this.ns.getGrowTime(server);
         let threads = this.getGrowThreads(server);
         threads = Math.min(this.getMaxThreads(scriptName), threads);
         let secEffect = this.ns.growthAnalyzeSecurity(threads);
-        let moneyMultiplier = 1; //use HackingFormulas.growPercent(server.hostname, threads, ns.getPlayer(), this.cores);
+        let moneyMultiplier = 1; //use HackingFormulas.growPercent(server, threads, ns.getPlayer(), this.cores);
         if(threads > 0)
         {
-            this.ns.exec(scriptName, this.ns.getHostname(), threads);
-            this.tm.push(new Task(server.hostname, scriptName, secEffect, moneyMultiplier, endTime, threads))
+            this.ns.exec(scriptName, this.homeServer, threads, server, this.pid++);
+            this.tm.push(new Task(server, scriptName, secEffect, moneyMultiplier, endTime, threads));
+            this.ns.print(this.ns.sprintf("Spawning %d %s threads on %s", threads, scriptName, server));
         }
     }
 
     hack(server)
     {
         let scriptName = "hack.js";
-        let endTime = this.ns.getHackTime(server.hostname);
+        let endTime = this.ns.getHackTime(server);
         let threads = this.getHackThreads(server);
         threads = Math.min(this.getMaxThreads(scriptName), threads);
         let secEffect = this.ns.hackAnalyzeSecurity(threads);
-        let moneyMultiplier = this.ns.hackAnalyze(server.hostname) * threads * this.ns.hackAnalyzeChance(server.hostname);
+        let moneyMultiplier = this.ns.hackAnalyze(server) * threads * this.ns.hackAnalyzeChance(server);
         if(threads > 0)
         {
-            this.ns.exec(scriptName, this.ns.getHostname(), threads);
-            this.tm.push(new Task(server.hostname, scriptName, secEffect, moneyMultiplier, endTime, threads))
+            this.ns.exec(scriptName, this.homeServer, threads, server, this.pid++);
+            this.tm.push(new Task(server, scriptName, secEffect, moneyMultiplier, endTime, threads));
+            this.ns.print(this.ns.sprintf("Spawning %d %s threads on %s", threads, scriptName, server));
         }
     }
 
     getMaxThreads(script)
     {
-        let remainingRam = this.homeServer.maxRam - this.homeServer.ramUsed;
-        let scriptRam = this.ns.getScriptRam(script, this.homeServer.hostname);
+        let homeServ = this.ns.getServer(this.homeServer);
+        let remainingRam = homeServ.maxRam - homeServ.ramUsed;
+        //this.ns.tprintf("    %f used out of %f", this.homeServer.ramUsed, this.homeServer.maxRam);
+        let scriptRam = this.ns.getScriptRam(script, this.homeServer);
+        //this.ns.tprintf("    Enough ram for %d %s threads", Math.floor(remainingRam/scriptRam), script);
         return Math.floor(remainingRam/scriptRam);
     }
     
 
     getWeakenThreads(server) 
     {
-        let endTime = this.ns.getWeakenTime(server.hostname);
+        let endTime = this.ns.getWeakenTime(server);
         let secLevel = this.getSecLevelAtTime(server, endTime);
-        let secDelta = secLevel - server.minDifficulty;
+        let secDelta = secLevel - this.ns.getServerMinSecurityLevel(server)
         let stWeakenEffect = this.ns.weakenAnalyze(1, this.cores);
+        // this.ns.tprint("  SecLevel: " + this.ns.getServerSecurityLevel(server));
+        // this.ns.tprint("  predicted SecLevel: " + secLevel);
+        // this.ns.tprint("  SecDelta pred: " + secDelta);
+        // this.ns.tprint("  singleThread weaken: " + stWeakenEffect);
+        // this.ns.tprint("  threads: " + Math.floor(secDelta / stWeakenEffect));
         return Math.floor(secDelta / stWeakenEffect);
     }
 
     getGrowThreads(server)
     {
         let growThreads = 0;
-        let endTime = this.ns.getGrowTime(server.hostname);
-        let effectiveMoneyPercentage = this.getMoneyPercentAtTime(server.hostname, endTime);
+        let endTime = this.ns.getGrowTime(server);
+        let effectiveMoneyPercentage = this.getMoneyPercentAtTime(server, endTime);
         if(effectiveMoneyPercentage < 1)
         {
             let growthPercent = 1 / effectiveMoneyPercentage;
-            growThreads = this.ns.growthAnalyze(sever.hostname, growthPercent, this.cores);
+            growThreads = this.ns.growthAnalyze(server, growthPercent, this.cores);
         }
-        return Math.floor(growThreads);        
+        return Math.floor(growThreads);
     }
 
     getHackThreads(server)
     {
+        let serv = this.ns.getServer(server);
         let targetHackPercentage = 0.25;
-        let endTime = this.ns.getHackTime(server.hostname);
-        let effectiveMoneyPercentage = this.getMoneyPercentAtTime(server.hostname, endTime);
+        let endTime = this.ns.getHackTime(server);
+        let effectiveMoneyPercentage = this.getMoneyPercentAtTime(server, endTime);
         let hackThreads = 0;
         if(effectiveMoneyPercentage > 0.98)
         {
-            let hackDollars = server.moneyMax * targetHackPercentage;
-            hackThreads = this.ns.hackAnalyzeThreads(server.hostname, hackDollars);
+            let hackDollars = serv.moneyMax * targetHackPercentage;
+            hackThreads = this.ns.hackAnalyzeThreads(server, hackDollars);
         }
         return Math.floor(hackThreads);        
     }
 
     getSecLevelAtTime(server, time) 
     {
-        let secLevel = server.hackDifficulty;
+        let serv = this.ns.getServer(server);
+        let secLevel = this.ns.getServerSecurityLevel(server);
         let tasks = this.tm.getTasks();
         for(let i = 0; i < tasks.length; i++)
         {
             let task = tasks[i];
-            if(task.serverName == server.hostname)
+            if(task.serverName == server)
             {
                 if(task.endTime < time)
                 {
                     secLevel += task.secEffect;
                 }
-                if(secLevel < server.minDifficulty)
+                if(secLevel < serv.minDifficulty)
                 {
-                    secLevel = server.minDifficulty;
+                    secLevel = serv.minDifficulty;
                 }
             }
         }
@@ -148,30 +184,32 @@ class Analyzer
 
     getMoneyPercentAtTime(server, time)
     {
-        let money = server.moneyAvailable;
+        let serv = this.ns.getServer(server);
+        let money = serv.moneyAvailable;
         let tasks = this.tm.getTasks();
         for(let i = 0; i < tasks.length; i++)
         {
             let task = tasks[i];
-            if(task.serverName == server.hostname)
+            if(task.serverName == server)
             {
                 if(task.endTime < time)
                 {
                     money *= task.moneyMultiplier;
                 }
-                if(money > server.moneyMax)
+                if(money > serv.moneyMax)
                 {
                     money = moneyMax;
                 }
             }
         }
-        return money / server.moneyMax;
+        return money / serv.moneyMax;
     }
 }
 
 function sortByEndTime(a, b)
 {
-    return b - a;
+    //this.ns.tprint("Sorting: " + a.endTime + " " + b.endTime);
+    return b.endTime - a.endTime;
 }
 
 class TaskManager 
@@ -179,32 +217,30 @@ class TaskManager
     constructor(ns) 
     {
         this.ns = ns;
-        this.tasklist = new Array();
+        this.myTasks = new Array();
     }
 
     push(task)
     {
-        this.tasklist.push(task);
+        ////this.ns.tprint("Pushing Task " + task);
+        this.myTasks.push(task);
     }
 
     getTasks()
     {
-        this.tasklist.sort(sortByEndTime);
-        for(var i = 0; i < this.tasklist.length; i++)
+        ////this.ns.tprint("Sorting tasklist: " + this.tasklist);
+        //this.tasklist = this.tasklist.sort(sortByEndTime);
+        for(var i = 0; i < this.myTasks.length; i++)
         {
-            let task = this.tasklist[i];
+            let task = this.myTasks[i];
             if(task.endTime < this.ns.getTimeSinceLastAug())
             {
                 //remove first item from the list.
-                this.tasklist = this.tasklist.shift();
+                this.myTasks.shift();
                 i--;
             }
-            else
-            {
-                break;
-            }
         }
-        return this.tasklist;
+        return this.myTasks;
     }
 }
 

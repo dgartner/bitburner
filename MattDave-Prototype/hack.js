10af9bb20e07de778fe353a6a3fb439ada51665d
Leichenhalle <MatthewDavid.Grove@Gmail.com>
2022-01-18 19:06:55 -0600

    Updated underlying action scripts.

--- MattDave-Prototype/hack.js
+++ MattDave-Prototype/hack.js
@@ -2,11 +2,6 @@
 export async function main(ns) 
 {
     let target = ns.args[0];
-    let actionId = ns.args[1];
 
-    let actionDetails = [target, actionId];
-
-    ns.sprintf("Running Hack Action: %s", JSON.stringify(actionDetails));
-
-    await ns.hack(target);
+    await ns.grow(target);
 }
--- MattDave-Prototype/weaken.js
+++ MattDave-Prototype/weaken.js
@@ -2,11 +2,6 @@
 export async function main(ns) 
 {
     let target = ns.args[0];
-    let actionId = ns.args[1];
 
-    let actionDetails = [target, actionId];
-
-    ns.sprintf("Running Weaken Action: %s", JSON.stringify(actionDetails));
-
-    await ns.weaken(target);
+    await ns.grow(target);
 }
